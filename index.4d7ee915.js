e26bb36dc7329744e9977f4caaf9e4ef60640e2d
Maria2009gift <117043895+Maria2009gift@users.noreply.github.com>
2023-12-20 16:53:37 +0000

    Deploying to gh-pages from @ Daniilkir/DreamGames_Project@fb31e4d8957e4f8ac36c8b24edd4484f048a08d0 🚀

---
+++ index.4d7ee915.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".number1"),n=document.querySelector(".number2"),u=document.querySelector(".number3"),r=document.querySelector(".span-3numbers");document.querySelector(".p-3numbers").addEventListener("click",(function(){for(var t=[e.value,n.value,u.value],c=0,o=0;o<t.length;o+=1)t[o]>c&&(c=t[o]);r.textContent=c}))}();
+//# sourceMappingURL=index.4d7ee915.js.map
---
+++ index.fd553086.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".number1"),t=document.querySelector(".number2"),n=document.querySelector(".number3"),u=document.querySelector(".span-3numbers");document.querySelector(".p-3numbers").addEventListener("click",(function(){const r=[e.value,t.value,n.value];let c=0;for(let e=0;e<r.length;e+=1)r[e]>c&&(c=r[e]);u.textContent=c}));
+//# sourceMappingURL=index.fd553086.js.map
